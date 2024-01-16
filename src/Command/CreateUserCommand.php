2f0e26893edb023af12dcf42b4d62bb2f55756aa
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-01-16 11:44:37 +0600

    CreateUserCommand.php updated

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -1,34 +1,42 @@
 <?php
 
-// src/Command/CreateUserCommand.php
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CreateUserCommand extends Command
 {
-    // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:create-user';
+    protected static $defaultName = 'create:user';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
 
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
 
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
         return Command::SUCCESS;
-
-        // or return this if some error happened during the execution
-        // (it's equivalent to returning int(1))
-        // return Command::FAILURE;
-
-        // or return this to indicate incorrect command usage; e.g. invalid options
-        // or missing arguments (it's equivalent to returning int(2))
-        // return Command::INVALID
     }
 }
