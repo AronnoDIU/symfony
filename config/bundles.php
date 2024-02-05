9edb5e811abb23664390c493f565b2866638b4c3
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-02-05 08:45:26 +0600

    Install friendsofsymfony/rest-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
