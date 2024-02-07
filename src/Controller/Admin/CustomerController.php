f97b2c278411435b4c26aac13c8aa130594802d3
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-02-07 09:28:43 +0600

    Generate Cache for CustomerIndex

--- src/Controller/Admin/CustomerController.php
+++ src/Controller/Admin/CustomerController.php
@@ -7,6 +7,9 @@ namespace App\Controller\Admin;
 use App\Entity\Customer;
 use App\Form\CustomerType;
 use App\Repository\CustomerRepository;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,19 +22,23 @@ class CustomerController extends AbstractController
 {
     /**
      * @Route("/", name="app_customer_index", methods={"GET"})
+     * @throws InvalidArgumentException
      */
-    public function index(CustomerRepository $customerRepository): Response
+    public function index(CustomerRepository $customerRepository, CacheInterface $cache): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('customer/index.html.twig', [
-            'customers' => $customerRepository->findAll(),
-        ]);
+        return $cache->get('customer_index', function () use ($customerRepository) {
+            return $this->render('customer/index.html.twig', [
+                'customers' => $customerRepository->findAll(),
+            ]);
+        });
     }
 
     /**
      * @Route("/new", name="app_customer_new", methods={"GET", "POST"})
+     * @throws InvalidArgumentException
      */
-    public function new(Request $request, CustomerRepository $customerRepository): Response
+    public function new(Request $request, CustomerRepository $customerRepository, CacheInterface $cache): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $customer = new Customer();
@@ -40,7 +47,8 @@ class CustomerController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $customerRepository->add($customer, true);
-
+            // Invalidate cache associated with customer index page
+            $cache->delete('customer_index');
             return $this->redirectToRoute('app_customer_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -49,6 +57,7 @@ class CustomerController extends AbstractController
             'form' => $form,
         ]);
     }
+
 
     /**
      * @Route("/{id}", name="app_customer_show", methods={"GET"})
@@ -88,7 +97,7 @@ class CustomerController extends AbstractController
     public function delete(Request $request, Customer $customer, CustomerRepository $customerRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        if ($this->isCsrfTokenValid('delete'.$customer->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $customer->getId(), $request->request->get('_token'))) {
             $customerRepository->remove($customer, true);
         }
 
