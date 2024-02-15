18a644fce8754562775c118cefe9b0eaf6fd3ca7
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-02-15 10:46:30 +0600

    Some Entities:Transfer created

---
+++ src/Entity/Transfer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TransferRepository::class)
+ */
+class Transfer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $status;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Transfer/Product.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity\Transfer;
+
+use App\Repository\Transfer\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $original;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $quantity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginal(): ?string
+    {
+        return $this->original;
+    }
+
+    public function setOriginal(string $original): self
+    {
+        $this->original = $original;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(string $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
